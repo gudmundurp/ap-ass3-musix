3f7f09a56d61262ae380b2f530a04a67cc5f6e9d
Gummi <g@g.g>
2012-10-24 20:52:01 +0200

    Magnad

--- tasks.erl
+++ tasks.erl
@@ -2,25 +2,37 @@
 -import(mr).
 -import(read_mxm).
 -export([run_tasks/0]).
-
-take(List, N) -> take(List, N, []).
-
-take([H|T], N, Acc) when N > 0 ->
-    take(T, N-1, [H|Acc]);
-take(_, 0, Acc) -> lists:reverse(Acc).
-
-countWords(Words) ->
-    io:format("Length of Words is ~p~n",[length(Words)]),
-    {ok, MR} = mr:start(4),
-    {ok, Res} = mr:job(MR,
+	
+run_tasks() ->
+    {Words,Tracks} = read_mxm:from_file("data/mxm_dataset_test.txt"),
+    
+	{ok, MR} = mr:start(4),
+	
+    {ok, C} = mr:job(MR,
         fun( Word ) -> Word end,
 	    fun( _, Count ) -> Count+1 end,
 	    0,
 	    Words
 	),
-    mr:stop(MR),
-    Res.
+    io:format("Total number of words: ~p~n",[C]),
 
-run_tasks() ->
-    {Words,_} = read_mxm:from_file("data/mxm_dataset_test.txt"),
-    countWords(Words).
+	L = length(Tracks),
+	io:format("Total number of tracks: ~p~n",[L]),
+    {ok,Av_diff_song} = mr:job(MR,
+	    fun( Track ) -> 
+		    {_,_,WordList} = read_mxm:parse_track(Track),
+			length(WordList)
+		end,
+		fun( Length, Acc ) -> Acc+Length/L end,
+		0,
+		Tracks
+	),
+	io:format("Average different words in a song: ~p~n",[Av_diff_song]),
+	
+	Av_total_song = C/L,
+	io:format("Average total nr. of words in a song: ~p~n",[Av_total_song]),
+	
+	
+	
+	mr:stop(MR).
+
